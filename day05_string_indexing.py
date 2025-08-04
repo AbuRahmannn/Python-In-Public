14cb7f332af25bc30a37c07514bf9bf6fccf540b
Abdul Rahman <ima.rahman06@gmail.com>
2025-08-04 22:01:51 +0530

    Add files via upload

---
+++ day05_string_indexing.py
@@ -0,0 +1,23 @@
+#string indexing
+#[start : end : step]
+
+
+num = "1234-5678-9012-3456"
+
+print("print the starting index of string")
+print(num[0])
+
+print("print end index of the string")
+print(num[ :4])
+
+print("print end of the string")
+print(num[5:9])
+
+print("print end string starting from 5")
+print(num[5: ])
+
+print("print end of the string")
+print(num[-1])
+
+print("print number by spacing 3 nums")
+print(num[: : 3])
