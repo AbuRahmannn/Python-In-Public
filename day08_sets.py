af988a99efdae63d66be7d113aa9cd35a869d6e1
Abdul Rahman <ima.rahman06@gmail.com>
2025-08-10 20:36:49 +0530

    Add files via upload

---
+++ day08_sets.py
@@ -0,0 +1,15 @@
+#day08   SET
+#no dublicates
+#add, remove and pop
+
+fruits = {"apple","orange","banana","coconut"}
+print(fruits)
+#print(dir(fruits))
+#print(help(fruits))
+#print(len(fruits))
+#print("pineapple" in fruits) boolean
+#fruits.add("pineapple")
+#fruits.remove("apple")
+#fruits.pop()
+#fruits.clear()
+
---
+++ day08_tuple.py
@@ -0,0 +1,12 @@
+#day08   TUPLE
+#dublicates ok and faster
+
+fruits = ("apple","orange","banana","coconut","banana")
+print(fruits)
+#print(dir(fruits))
+#print(help(fruits))
+#print(len(fruits))
+#print("pineapple" in fruits) boolean
+
+print(fruits.index("banana")) #to count the index
+print(fruits.count("banana")) #to count how many times it repeated
